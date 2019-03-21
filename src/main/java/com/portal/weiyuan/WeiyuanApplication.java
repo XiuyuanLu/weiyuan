b21ee0879eb8e0cd8c70591d9dc52c5fd719d9d1
XiuyuanLu <27690@DESKTOP-S7L88TO>
2019-03-21 23:54:29 +0800

    init

---
+++ src/main/java/com/portal/weiyuan/WeiyuanApplication.java
@@ -0,0 +1,13 @@
+package com.portal.weiyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiyuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeiyuanApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/portal/weiyuan/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.portal.weiyuan.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("index")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
+	
+}
